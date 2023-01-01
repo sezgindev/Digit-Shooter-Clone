b2f155d2ec88b76020dfb8013554d16a97bfff5d
Sezgin Yilmaz <sezginyilmazdev@gmail.com>
2023-01-01 23:28:24 +0300

    collectable digit increase logic added

--- DigitShooterClone/Assets/GAME/Scripts/Interactables/CollectableDigit.cs
+++ DigitShooterClone/Assets/GAME/Scripts/Interactables/CollectableDigit.cs
@@ -9,6 +9,9 @@ public class CollectableDigit : MonoBehaviour
     private const int _bulletDigitLayer = 8;
     private const int _playerLayer = 6;
     [SerializeField] private float _currentNumber = 10;
+    [SerializeField] private List<GameObject> digits;
+    [SerializeField] private List<GameObject> _currentDigits;
+
 
     private void OnTriggerEnter(Collider other)
     {
@@ -34,6 +37,37 @@ public class CollectableDigit : MonoBehaviour
 
     private void ChangeNumberModel(float newNumber)
     {
-        //do something
+        foreach (var digit in _currentDigits)
+        {
+            Destroy(digit);
+        }
+
+        int digitCount = newNumber.ToString().Length;
+        List<GameObject> spawnableDigits = new List<GameObject>();
+        _currentDigits = new List<GameObject>();
+        for (int i = 0; i < digitCount; i++)
+        {
+            int a = (int)newNumber.ToString()[i] - '0';
+            spawnableDigits.Add(digits[a]);
+            var digit = Instantiate(spawnableDigits[i], spawnableDigits[i].transform.position,
+                Quaternion.Euler(0, 180, 0),
+                transform);
+            _currentDigits.Add(digit);
+        }
+
+        DigitRePositioning();
+    }
+
+    private void DigitRePositioning()
+    {
+        //+0.2
+        var xOffset = 0.0f;
+        for (int i = 0; i < _currentDigits.Count; i++)
+        {
+            var newPos = _currentDigits[i].transform.position;
+            newPos.x += xOffset;
+            _currentDigits[i].transform.position = newPos;
+            xOffset += 0.2f;
+        }
     }
 }
